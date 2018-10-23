7116e1906e7df80b3ff3ece99ba030a42975c28b
eckmLJE <lucas.eckman@gmail.com>
2018-10-23 13:11:44 -0400

    tech int

--- src/containers/Bio.js
+++ src/containers/Bio.js
@@ -6,9 +6,40 @@ const Bio = () => {
     <section className="bio">
       <div className="bio-content">
         <p>
-          <strong>Lucas Eckman</strong> is a full-stack web developer and urban
-          design consultant living in Washington, DC.
+          <strong>Lucas Eckman</strong> is a full-stack web developer, bicycle
+          courier, and urban design consultant living in Washington, DC.
         </p>
+      </div>
+      <div className="bio-cards">
+        <div className="bio-card">
+          <div className="bio-card-inner">
+            <h1>TECHNOLOGIES</h1>
+            <ul>
+              <li>JavaScript ES6</li>
+              <li>React.JS</li>
+              <li>HTML5 + CSS3</li>
+              <li>Ruby + Rails</li>
+              <li>Redux, Router, Thunk</li>
+              <li>PostgreSQL, SQLite</li>
+              <li>Bootstrap, Semantic, Ant Design</li>
+              <li>SVG</li>
+              <li>Unity 3D, Google Cardboard</li>
+            </ul>
+          </div>
+        </div>
+        <div className="bio-card">
+          <div className="bio-card-inner">
+            <h1>INTERESTS</h1>
+            <ul>
+              <li>Urban Design + Transportation</li>
+              <li>Cycling</li>
+              <li>Hiking + Camping</li>
+              <li>Travel</li>
+              <li>Esports</li>
+              <li>My dog named Lady</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </section>
   );
